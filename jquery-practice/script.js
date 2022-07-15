65aac499c250a1c2bda22a4da806c8bb35089cd0
Anoop Bharadwaj <bharadwajanoop@yahoo.com>
2022-07-15 16:24:42 +0530

    Added script2.js file and modified functionality by storing values in local storage to get output

--- jquery-practice/script.js
+++ jquery-practice/script.js
@@ -1,3 +1,20 @@
-// document.querySelector('.btn').addEventListener('click', function (e) {
-//   e.preventDefault();
-// });
+$('#login-btn').click(function (e) {
+  //   e.preventDefault();
+
+  let enteredEmail = document.querySelector('#email').value;
+  let enteredPassword = document.querySelector('#password').value;
+
+  if (!enteredEmail || !enteredPassword) {
+    alert('Please enter all fields');
+  } else {
+    $('form').attr('action', 'successpage.html');
+    const emailInput = $('#email').val();
+    localStorage.setItem('email', emailInput);
+
+    console.log(emailInput);
+
+    const passwordInput = $('#password').val();
+    localStorage.setItem('password', passwordInput);
+    console.log(passwordInput);
+  }
+});
